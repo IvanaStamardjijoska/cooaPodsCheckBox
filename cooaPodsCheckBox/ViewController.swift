8874454ae61e3393a2bc9add6556bbd600c11268
CodeWell <ivanastamardjioska@gmail.com>
2019-01-17 17:11:32 +0100

    Initial Commit

---
+++ cooaPodsCheckBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cooaPodsCheckBox
+//
+//  Created by CodeWell on 10/29/18.
+//  Copyright © 2018 Ivana Stamardjioska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
